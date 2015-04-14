de3749ab0a5ef01918db902570a8d2dd203fb6a9
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2015-04-14 10:56:19 +0200

    Add function to print all possible styles.. and executed it 10 times -> slow

--- examples/main.rs
+++ examples/main.rs
@@ -10,10 +10,14 @@ struct Foo {
 }
 
 fn main() {
-  let x = Foo { bar: "huhu".to_string() };
+    for _ in 0..10 {
+        all_styles();
+    }
 
-  println!("{:?}", Plain.paint(&x));
-  println!("{:?}", Plain.paint(x));
+  // let x = Foo { bar: "huhu".to_string() };
+
+  // println!("{:?}", Plain.paint(&x));
+  // println!("{:?}", Plain.paint(x));
 
   // println!("{}\n{}\n{}\n{}\n{}",
   //   Red.bg(Green).bold().paint("Red-Green-Bold"),
@@ -21,4 +25,33 @@ fn main() {
   //   Blue.bold().paint("Blue"),
   //   Blue.bg(Magenta).paint("Blue"),
   //   Normal.underline().paint("Underline"));
+}
+
+fn all_styles() {
+    let colors =
+        [Normal, Black, Red, Green, Yellow, Blue, Magenta, Cyan, White];
+
+    // Normal test
+    for c in &colors { print!("{:?} ", c.paint(c)); }
+    println!("    (fg)");
+    for c in &colors { print!("{:?} ", Plain.bg(*c).paint(c)); }
+    println!("    (bg)");
+
+    // Bold text
+    for c in &colors { print!("{:?} ", c.bold().paint(c)); }
+    println!("    (bold fg)");
+    for c in &colors { print!("{:?} ", Bold.bg(*c).paint(c)); }
+    println!("    (bold bg)");
+
+    // Underlined text
+    for c in &colors { print!("{:?} ", c.underline().paint(c)); }
+    println!("    (underline fg)");
+    for c in &colors { print!("{:?} ", Underline.bg(*c).paint(c)); }
+    println!("    (underline bg)");
+
+    // Underlined and bold text
+    for c in &colors { print!("{:?} ", c.underline().bold().paint(c)); }
+    println!("    (underline bold fg)");
+    for c in &colors { print!("{:?} ", Underline.bg(*c).bold().paint(c)); }
+    println!("    (underline bold bg)");
 }
